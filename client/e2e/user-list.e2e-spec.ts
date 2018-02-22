bc7f2bd365e3e769a96af32baa636493cfd56292
Nic McPhee <mcphee@morris.umn.edu>
2018-02-22 01:30:32 -0600

    Add delay so E2E tests pass
    This comments out the delay at the top of these E2E tests, which then
    causes one of the other tests to fail because we jump to the expect
    before the user has been fully added and updated. I had to add a 10
    second delay to get this to pass consistently.

--- client/e2e/user-list.e2e-spec.ts
+++ client/e2e/user-list.e2e-spec.ts
@@ -5,18 +5,18 @@ import {Key} from "selenium-webdriver";
 let origFn = browser.driver.controlFlow().execute;
 
 // https://hassantariqblog.wordpress.com/2015/11/09/reduce-speed-of-angular-e2e-protractor-tests/
-browser.driver.controlFlow().execute = function () {
-    let args = arguments;
-
-    // queue 100ms wait between test
-    // This delay is only put here so that you can watch the browser do its thing.
-    // If you're tired of it taking long you can remove this call
-    origFn.call(browser.driver.controlFlow(), function () {
-        return protractor.promise.delayed(100);
-    });
-
-    return origFn.apply(browser.driver.controlFlow(), args);
-};
+// browser.driver.controlFlow().execute = function () {
+//     let args = arguments;
+//
+//     // queue 100ms wait between test
+//     // This delay is only put here so that you can watch the browser do its thing.
+//     // If you're tired of it taking long you can remove this call
+//     origFn.call(browser.driver.controlFlow(), function () {
+//         return protractor.promise.delayed(100);
+//     });
+//
+//     return origFn.apply(browser.driver.controlFlow(), args);
+// };
 
 describe('User list', () => {
     let page: UserPage;
@@ -120,7 +120,12 @@ describe('User list', () => {
         element(by.id('companyField')).sendKeys('Awesome Startup, LLC');
         element(by.id('emailField')).sendKeys('tracy@awesome.com');
         element(by.id('confirmAddUserButton')).click();
-        expect(page.getUniqueUser('tracy@awesome.com')).toMatch('Tracy Kim.*'); // toEqual('Tracy Kim');
+        // This annoying delay is necessary, otherwise it's possible that we execute the `expect`
+        // line before the add user has been fully processed and the new user is available
+        // in the list.
+        setTimeout(() => {
+            expect(page.getUniqueUser('tracy@awesome.com')).toMatch('Tracy Kim.*'); // toEqual('Tracy Kim');
+        }, 10000);
     });
 
     it('Should allow us to put information into the fields of the add user dialog', () => {
