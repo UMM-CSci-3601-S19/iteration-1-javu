8d27e18df23301dc37209b641c140b968ee4ce5e
Nic McPhee <mcphee@morris.umn.edu>
2019-02-15 16:53:31 -0600

    Update node version
    We were using a *very* old version of `node` (v6.11.2) and need to
    update that to be able to use more recent version of important tools
    like Angular CLI.

--- client/e2e/user-list.e2e-spec.ts
+++ client/e2e/user-list.e2e-spec.ts
@@ -13,7 +13,8 @@ browser.driver.controlFlow().execute = function () {
 
     // queue 100ms wait between test
     // This delay is only put here so that you can watch the browser do its thing.
-    // If you're tired of it taking long you can remove this call
+    // If you're tired of it taking long you can remove this call or change the delay
+    // to something smaller (even 0).
     origFn.call(browser.driver.controlFlow(), () => {
         return protractor.promise.delayed(100);
     });
