9544134f38c8ddbe50872e41781c1129f2b47c87
UtkarshKr007 <kumar375@morris.umn.edu>
2019-03-07 15:42:11 -0600

    Create ride-list.service.ts and method getRides()

---
+++ client/src/app/rides/ride-list.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment';
+import {Ride} from "./ride";
+
+@Injectable()
+export class RideListService {
+  readonly baseUrl: string = environment.API_URL + 'rides';
+  private rideUrl: string = this.baseUrl;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getRides(): Observable<Ride[]> {
+    return this.http.get<Ride[]>(this.rideUrl);
+  }
+
+}
