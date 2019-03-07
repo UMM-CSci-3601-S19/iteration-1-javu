433865bdd0c723bfe99a003e0e0525dc6c10b1e2
UtkarshKr007 <kumar375@morris.umn.edu>
2019-03-07 15:57:26 -0600

    Create css & html file for ride.component. Add @Component to .ts file

--- client/src/app/rides/ride.component.ts
+++ client/src/app/rides/ride.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {RideListService} from "./ride-list.service";
 import {Ride} from "./ride";
 
+@Component({
+  selector: 'ride-component',
+  templateUrl: 'ride.component.html',
+  styleUrls: ['./ride.component.css']
+})
 
 export class RideComponent implements OnInit {
   public ride: Ride = null;
