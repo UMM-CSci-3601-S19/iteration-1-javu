730b2aaa74438baa737dcf1002f4e827836bd8b9
UtkarshKr007 <kumar375@morris.umn.edu>
2019-03-08 10:56:33 -0600

    Add imports for RideComponent, RideListComponent and RideListService in app.module

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,7 +3,8 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './users/user-list.component';
-import {RideListComponent} from "./rides/rider-list.component";
+import {RideListComponent} from "./rides/ride-list.component";
+
 
 // Route Configuration
 export const routes: Routes = [
