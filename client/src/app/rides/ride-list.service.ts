3eebf7960a00d95c2a1c753db9c2dcd880287799
UtkarshKr007 <kumar375@morris.umn.edu>
2019-03-09 15:14:44 -0600

    Create addNewRide service in ride-list.service.ts

--- client/src/app/rides/ride-list.service.ts
+++ client/src/app/rides/ride-list.service.ts
@@ -21,4 +21,19 @@ export class RideListService {
     return this.http.get<Ride>(this.rideUrl + '/' + driver);
   }
 
+  addNewRide(newRide: Ride): Observable<string> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // We're sending JSON
+        'Content-Type': 'application/json'
+      }),
+      // But we're getting a simple (text) string in response
+      // The server sends the hex version of the new ride back
+      // so we know how to find/access that ride again later.
+      responseType: 'text' as 'json'
+    };
+
+    // Send post request to add a new ride with the ride data as the body with specified headers.
+    return this.http.post<string>(this.rideUrl + '/new', newRide, httpOptions);
+  }
 }
