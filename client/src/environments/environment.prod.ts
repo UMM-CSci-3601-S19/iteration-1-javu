ae2e3c275c586fed1dc4f2b0d4303f439c26a63f
Nic McPhee <mcphee@morris.umn.edu>
2019-03-11 09:35:35 -0500

    Remove Digital Ocean IP address
    This removes the Digital Ocean IP address, and adds a
    comment clarifying the role of the port number.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,6 +3,9 @@ export const environment = {
   production: true,
   // You need to replace the IP address below with the
   // IP address of your droplet, which you can get
-  // from the Digital Ocean dashboard.
-  API_URL: 'http://138.197.219.82:4567/api/'
+  // from the Digital Ocean dashboard. Make sure you
+  // keep the port number (`:4567`) unless you
+  // reconfigure the Java Spark server to use a
+  // different port.
+  API_URL: 'http://xxx.yyy.zzz.www:4567/api/'
 };
