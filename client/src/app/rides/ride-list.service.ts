03eb1c196dabe48958f759e279d41e30da53e880
UtkarshKr007 <kumar375@morris.umn.edu>
2019-03-11 21:32:52 -0500

    Add the optional field of rideDestination to getRides and create method filterByDestiantion as well as parameterPresent and removeParameter for testing

--- client/src/app/rides/ride-list.service.ts
+++ client/src/app/rides/ride-list.service.ts
@@ -13,12 +13,58 @@ export class RideListService {
 
   }
 
-  getRides(): Observable<Ride[]> {
+  getRides(rideDestination?: string): Observable<Ride[]> {
+    this.filterByDestination(rideDestination);
     return this.http.get<Ride[]>(this.rideUrl);
   }
 
+
+  //This could be changed into a getRideById if we decide to ad id as a field
   getRideByDestination(destination: string): Observable<Ride> {
     return this.http.get<Ride>(this.rideUrl + '/' + destination);
+  }
+
+
+  filterByDestination(rideDestination?: string): void {
+    if (!(rideDestination == null || rideDestination === '')) {
+      if (this.parameterPresent('destination=')) {
+        // there was a previous search by destination that we need to clear
+        this.removeParameter('destination=');
+      }
+      if (this.rideUrl.indexOf('?') !== -1) {
+        // there was already some information passed in this url
+        this.rideUrl += 'destination=' + rideDestination + '&';
+      } else {
+        // this was the first bit of information to pass in the url
+        this.rideUrl += '?destination=' + rideDestination + '&';
+      }
+    } else {
+      // there was nothing in the box to put onto the URL... reset
+      if (this.parameterPresent('destination=')) {
+        let start = this.rideUrl.indexOf('destination=');
+        const end = this.rideUrl.indexOf('&', start);
+        if (this.rideUrl.substring(start - 1, start) === '?') {
+          start = start - 1;
+        }
+        this.rideUrl = this.rideUrl.substring(0, start) + this.rideUrl.substring(end + 1);
+      }
+    }
+  }
+
+  private parameterPresent(searchParam: string) {
+    return this.rideUrl.indexOf(searchParam) !== -1;
+  }
+
+  //remove the parameter and, if present, the &
+  private removeParameter(searchParam: string) {
+    let start = this.rideUrl.indexOf(searchParam);
+    let end = 0;
+    if (this.rideUrl.indexOf('&') !== -1) {
+      end = this.rideUrl.indexOf('&', start) + 1;
+    } else {
+      end = this.rideUrl.indexOf('&', start);
+    }
+    this.rideUrl = this.rideUrl.substring(0, start) + this.rideUrl.substring(end);
   }
 
   addNewRide(newRide: Ride): Observable<string> {
