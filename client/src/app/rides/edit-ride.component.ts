611e99495cc016fbc2a576e22ef36395d764274c
UtkarshKr007 <kumar375@morris.umn.edu>
2019-03-12 18:55:30 -0500

    Create edit-ride.component.ts

---
+++ client/src/app/rides/edit-ride.component.ts
@@ -0,0 +1,86 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
+import {Ride} from './ride';
+import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
+import {DestinationValidator} from './destination.validator';
+
+@Component({
+  selector: 'edit-ride.component',
+  templateUrl: 'edit-ride.component.html',
+})
+
+export class EditRideComponent implements OnInit {
+  editRideForm: FormGroup;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { ride: Ride }, private fb: FormBuilder) {
+  }
+
+  edit_ride_validation_messages = {
+    'destination': [
+      {type: 'required', message: 'Destination is required'},
+      {type: 'minlength', message: 'Destination must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Destination cannot be more than 100 characters long'}
+    ],
+    'driver': [
+      {type: 'required', message: 'Driver is required'},
+      {type: 'minlength', message: 'Driver must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Driver cannot be more than 50 characters long'},
+      {type: 'pattern', message: 'Driver must contain only numbers and letters'}
+    ],
+    'origin': [
+      {type: 'required', message: 'Origin is required'},
+      {type: 'minlength', message: 'Origin must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Origin cannot be more than 100 characters long'}
+    ],
+    'departureTime': [
+      {type: 'required', message: 'DepartureTime is required'},
+      {type: 'minlength', message: 'DepartureTime must be at least 2 characters long'},
+      {type: 'maxlength', message: 'DepartureTime cannot be more than 100 characters long'}
+    ],
+    'notes': [
+      {type: 'required', message: 'Notes is required'},
+      {type: 'minlength', message: 'Notes must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Notes cannot be more than 150 characters long'}
+    ]
+  };
+
+  createForms() {
+    this.editRideForm = this.fb.group({
+      destination: new FormControl('destination', Validators.compose([
+        //Must make a destination.validator.ts file for this next line to work
+        DestinationValidator.validDestination,
+        Validators.minLength(2),
+        Validators.maxLength(100),
+        Validators.required
+      ])),
+      driver: new FormControl('driver', Validators.compose([
+        Validators.minLength(2),
+        Validators.maxLength(50),
+        Validators.required,
+        Validators.pattern('^[A-Za-z0-9\\s]+[A-Za-z0-9\\s]+$(\\.0-9+)?')
+      ])),
+      origin: new FormControl('origin', Validators.compose([
+        Validators.minLength(2),
+        Validators.maxLength(100),
+        Validators.required
+      ])),
+      departureTime: new FormControl('departureTime', Validators.compose([
+        Validators.minLength(2),
+        Validators.maxLength(100),
+        Validators.required
+      ])),
+      notes: new FormControl('notes', Validators.compose([
+        Validators.minLength(2),
+        Validators.maxLength(150),
+        Validators.required
+      ]))
+
+    })
+  }
+
+  ngOnInit() {
+    this.createForms();
+  }
+
+}
