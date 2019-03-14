6bf8196217ad6028dbd3837df04ba7d9e4d0a40e
Jackson Tenor <tenor031@umn.edu>
2019-03-14 11:06:04 -0500

    Add delay after sending keys in add ride e2e test
    It passes now
    with @utkarshkr007

--- client/e2e/ride-list.e2e-spec.ts
+++ client/e2e/ride-list.e2e-spec.ts
@@ -12,7 +12,7 @@ browser.driver.controlFlow().execute = function () {
  // If you're tired of it taking long you can remove this call or change the delay
  // to something smaller (even 0).
  origFn.call(browser.driver.controlFlow(), () => {
-   return protractor.promise.delayed(1);
+   return protractor.promise.delayed(10);
  });
 
  return origFn.apply(browser.driver.controlFlow(), args);
@@ -58,11 +58,17 @@ describe('Ride List', () => {
 
     it('Should actually add the ride with the information we put in the fields', () => {
       page.field('destinationField').sendKeys('New York');
+      protractor.promise.delayed(100);
       page.field('driverField').sendKeys('Bobbbo Billy');
+      protractor.promise.delayed(100);
       page.field('departureTimeField').sendKeys('In the morning');
+      protractor.promise.delayed(100);
       page.field('originField').sendKeys('Morris');
+      protractor.promise.delayed(100);
       page.field('notesField').sendKeys('I do not pick up my trash');
+      protractor.promise.delayed(100);
       page.click('roundTripCheckBox');
+      protractor.promise.delayed(100);
       expect(page.button('confirmAddRideButton').isEnabled()).toBe(true);
       page.click('confirmAddRideButton');
 
@@ -75,4 +81,4 @@ describe('Ride List', () => {
 
   });
 
-}); 
+});
